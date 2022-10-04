6a880cbd3433f30afe853c1d5b7d2c04bcf67d5d
khalid-tian <114968951+khalid-tian@users.noreply.github.com>
2022-10-04 16:11:04 -0300

    Revert "Add workflow"

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,10 +1,10 @@
 import autoExternal from 'rollup-plugin-auto-external';
 import tsPlugin from '@rollup/plugin-typescript';
 import resolve from "@rollup/plugin-node-resolve";
-import { terser } from 'rollup-plugin-terser';
+import commonjs from "@rollup/plugin-commonjs";
+// import { terser } from 'rollup-plugin-terser';
 import json from '@rollup/plugin-json';
 import pkg from "./package.json";
-import tsconfig from './tsconfig.json';
 
 export default [
     {
@@ -17,13 +17,14 @@ export default [
             },
         ],
         plugins: [
-            autoExternal(),
-            tsPlugin(tsconfig),
-            resolve({
-                preferBuiltins: true
-            }),
             json(),
-            terser()
+            tsPlugin(),
+            commonjs(),
+            autoExternal(),
+            resolve({
+                preferBuiltins: false
+            }),
+            // terser()
         ],
     },
 ];
